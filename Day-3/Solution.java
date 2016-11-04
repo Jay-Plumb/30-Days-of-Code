549efa90ea51df6cc59b9bd8a6d1d1eb00948580
Jay-Plumb <jaymjplumb@gmail.com>
2016-11-04 14:55:13 +0000

    added correct solution

--- Day-3/Solution.java
+++ Day-3/Solution.java
@@ -10,15 +10,27 @@ public class Solution {
       int n = scan.nextInt(); 
       scan.close();
       String ans="";
-          
-      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
-      if(n%2==1){
-         ans = "Weird";
+      if (n >= 1 && n <= 100) {  
+        // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+        if(n%2==1) {
+            ans = "Weird";
+        }
+        else {
+         // Complete the code
+            if (n%2 == 0) {
+                if (n >= 2 && n <= 5) {
+                    System.out.println("Not Weird");
+                }
+                if (n >= 6 && n <= 20) {
+                    System.out.println("Weird");
+                }
+                if (n > 20) {
+                    System.out.println("Not Weird");
+                }
+            }
+        }
+        System.out.println(ans);
       }
-      else{
-         // Complete the code 
-      }
-      System.out.println(ans);
    }
 }
 
